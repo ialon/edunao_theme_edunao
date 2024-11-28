fa9d52e38e9b52f5e9df5ec5087438c7c96071aa
Josemaria Bolanos <josemabol@gmail.com>
2024-11-28 17:42:24 -0500

    Add french translations

---
+++ lang/fr/theme_edunao.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Edunao - Language pack
+ *
+ * @package    theme_edunao
+ * @copyright  2024 Mako Digital <admin@mako.digital>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Général.
+$string['pluginname'] = 'Edunao';
+$string['choosereadme'] = 'Le thème Edunao est un thème enfant simplifié de Boost Union.';
+$string['configtitle'] = 'Edunao';
+$string['generalsettings'] = 'Paramètres généraux';
+
+// Paramètres : Chaînes générales.
+$string['myprofile_categories'] = 'Catégories de mon profil';
+$string['myprofile_categories_desc'] = 'Sélectionnez les catégories que vous souhaitez afficher dans la page Mon Profil.';
+$string['display_category_title'] = 'Afficher le titre de la catégorie';
+$string['display_category_title_desc'] = 'Sélectionnez si vous souhaitez afficher les titres des catégories dans la page Mon Profil.';
+$string['restrict_preferences'] = 'Restreindre l\'accès à la page des préférences';
+$string['restrict_preferences_desc'] = 'Supprime les liens vers la page des Préférences et redirige les utilisateurs vers la page Mon Profil s\'ils essaient d\'y accéder directement (sauf les administrateurs)';
+$string['hide_breadcrumbs'] = 'Masquer les fil d\'Ariane';
+$string['hide_breadcrumbs_desc'] = 'Masquer les fil d\'Ariane dans l\'en-tête de la page.';
+$string['hide_mainnavbar'] = 'Masquer la barre de navigation principale';
+$string['hide_mainnavbar_desc'] = 'Masquer la barre de navigation principale dans l\'en-tête de la page.';
