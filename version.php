de265120bf49344050cf8243c34273d0cc19d2f7
Josemaria Bolanos <jbolanos@mindfieldconsulting.com>
2024-11-28 12:55:00 -0500

    Initial commit

---
+++ version.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Edunao - Version file
+ *
+ * @package    theme_edunao
+ * @copyright  2024 Mako Digital <admin@mako.digital>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'theme_edunao';
+$plugin->version = 2023102050;
+$plugin->release = 'v4.3-r19';
+$plugin->requires = 2023100900;
+$plugin->supported = [403, 403];
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = [
+    'theme_boost' => 2023100900,
+    'theme_boost_union' => 2023102047
+];
