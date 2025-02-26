cc0964042e8154737f774041d9397260c77eff54
Josemaria Bolanos <josemabol@gmail.com>
2025-02-25 18:14:17 -0600

    Added italian and spanish translations

---
+++ lang/es/theme_edunao123.php
@@ -0,0 +1,67 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Edunao - Language pack
+ *
+ * @package    theme_edunao123
+ * @copyright  2024 Mako Digital <admin@mako.digital>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// General.
+$string['pluginname'] = 'Edunao';
+$string['choosereadme'] = 'El tema Edunao es un tema hijo de Boost Union simplificado.';
+$string['configtitle'] = 'Edunao';
+$string['generalsettings'] = 'Configuraciones Generales';
+
+// Settings: General strings.
+$string['myprofile_categories'] = 'Categorías de página de Perfil';
+$string['myprofile_categories_desc'] = 'Seleccione las categorías que desea mostrar en la página de Perfil.';
+$string['hide_category_title'] = 'Ocultar título de las categorías';
+$string['hide_category_title_desc'] = 'Seleccione si desea ocultar los títulos de las categorías en la página de Perfil.';
+$string['restrict_preferences'] = 'Restringir acceso a la página de preferencias';
+$string['restrict_preferences_desc'] = 'Elimina los enlaces a la página de Preferencias y redirige a los usuarios a la página de Perfil si intentan acceder directamente (excepto administradores)';
+$string['restrict_my_certificates'] = 'Restringir acceso a la página de Mis Certificados';
+$string['restrict_my_certificates_desc'] = 'Redirige a los usuarios a la página de Mi Perfil si intentan acceder directamente a la página de Mis Certificados (excepto administradores)';
+$string['hide_breadcrumbs'] = 'Ocultar migas de pan';
+$string['hide_breadcrumbs_desc'] = 'Ocultar migas de pan en el perfil de usuario y en las páginas de actividades del curso.';
+$string['hide_mainnavbar'] = 'Ocultar barra de navegación principal';
+$string['hide_mainnavbar_desc'] = 'Ocultar la barra de navegación principal en el encabezado de la página.';
+$string['hide_courseindex'] = 'Ocultar índice del curso';
+$string['hide_courseindex_desc'] = 'Ocultar el índice del curso en las páginas de actividades.';
+$string['hide_secondarynavigation'] = 'Ocultar navegación secundaria';
+$string['hide_secondarynavigation_desc'] = 'Oculta el menú de navegación secundaria del curso en las páginas de actividades.';
+
+// My certificates.
+$string['mycertificates'] = 'Mis certificados';
+$string['thumbnail'] = 'Miniatura';
+$string['nameandtime'] = 'Nombre y fecha de emisión';
+$string['courselink'] = 'Enlace al curso';
+$string['issuedon'] = 'Emitido el {$a}';
+$string['viewcourse'] = 'Ver curso';
+$string['downloadcertificate'] = 'Descargar certificado';
+
+// Login page
+$string['potentialidps'] = 'Iniciar sesión con:';
+
+// Navigation
+$string['back'] = 'Atrás';
+$string['edit'] = 'Editar';
+$string['settings'] = 'Configuraciones';
+
---
+++ lang/it/theme_edunao123.php
@@ -0,0 +1,67 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme Edunao - Language pack
+ *
+ * @package    theme_edunao123
+ * @copyright  2024 Mako Digital <admin@mako.digital>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Generale.
+$string['pluginname'] = 'Edunao';
+$string['choosereadme'] = 'Il tema Edunao è un tema figlio semplificato di Boost Union.';
+$string['configtitle'] = 'Edunao';
+$string['generalsettings'] = 'Impostazioni generali';
+
+// Impostazioni: Stringhe generali.
+$string['myprofile_categories'] = 'Le mie categorie di profilo';
+$string['myprofile_categories_desc'] = 'Seleziona le categorie che desideri visualizzare nella pagina del Mio Profilo.';
+$string['hide_category_title'] = 'Nascondi il titolo della categoria';
+$string['hide_category_title_desc'] = 'Seleziona se nascondere i titoli delle categorie nella pagina del Mio Profilo.';
+$string['restrict_preferences'] = 'Limita l\'accesso alla pagina delle preferenze';
+$string['restrict_preferences_desc'] = 'Rimuove i link alla pagina delle Preferenze e reindirizza gli utenti alla pagina del Mio Profilo se tentano di accedervi direttamente (eccetto gli amministratori)';
+$string['restrict_my_certificates'] = 'Limita l\'accesso alla pagina dei Miei Certificati';
+$string['restrict_my_certificates_desc'] = 'Reindirizza gli utenti alla pagina del Mio Profilo se tentano di accedere direttamente alla pagina dei Miei Certificati (eccetto gli amministratori)';
+$string['hide_breadcrumbs'] = 'Nascondi i breadcrumb';
+$string['hide_breadcrumbs_desc'] = 'Nascondi i breadcrumb nel profilo utente e nelle pagine delle attività del corso.';
+$string['hide_mainnavbar'] = 'Nascondi la barra di navigazione principale';
+$string['hide_mainnavbar_desc'] = 'Nascondi la barra di navigazione principale nell\'intestazione della pagina.';
+$string['hide_courseindex'] = 'Nascondi l\'indice del corso';
+$string['hide_courseindex_desc'] = 'Nascondi il cassetto dell\'indice del corso nelle pagine delle attività.';
+$string['hide_secondarynavigation'] = 'Nascondi la navigazione secondaria';
+$string['hide_secondarynavigation_desc'] = 'Nascondi il menu di navigazione secondaria del corso nelle pagine delle attività.';
+
+// I miei certificati.
+$string['mycertificates'] = 'I miei certificati';
+$string['thumbnail'] = 'Miniatura';
+$string['nameandtime'] = 'Nome e data di emissione';
+$string['courselink'] = 'Link al corso';
+$string['issuedon'] = 'Emesso {$a}';
+$string['viewcourse'] = 'Visualizza corso';
+$string['downloadcertificate'] = 'Scarica certificato';
+
+// Pagina di login
+$string['potentialidps'] = 'Accedi con:';
+
+// Navigazione
+$string['back'] = 'Indietro';
+$string['edit'] = 'Modifica';
+$string['settings'] = 'Impostazioni';
+
